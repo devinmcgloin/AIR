58d33d9d24f5a646ffea83d964f7939571c6ad45
Devin McGloin <devin@devinmcgloin.com>
2015-08-28 19:10:53 -0400

    cleared out reader, and other small changes

--- src/funct/Reader.java
+++ src/funct/Reader.java
@@ -1,7 +1,6 @@
 package funct;
 
 import org.apache.log4j.Logger;
-import util.Expression;
 
 /**
  * Created by devinmcgloin on 8/4/15.
@@ -13,24 +12,5 @@ public final class Reader {
 
     private Reader(){}
 
-    public static Expression parseExpression(String expression){
-        String[] terms = expression.trim().split(" ");
-        // [ 12 - 324 ft ]
-        //TODO fix this, it doesnt do what we want it to.
-        if(expression.startsWith("[")) {
-//            if (terms.length == 5)
-//                return new Expression(terms[0], terms[1], terms[2], terms[3]);
-//            else if (terms.length == 4)
-//                return new Expression(terms[0], terms[1], terms[2], "count");
-        }
-        // length < 25 ft
-        else{
-            if(terms.length == 4)
-                return new Expression(terms[0], terms[1], terms[2], terms[3]);
-            else if(terms.length == 3)
-                return new Expression(terms[0], terms[1], terms[2], "count");
-        }
-        return null;
-    }
 
 }
