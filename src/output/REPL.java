aa5f0194a8b57089bd2e095f06a3c25c84e99122
Devin McGloin <devin@devinmcgloin.com>
2015-09-21 00:54:31 -0400

    added help message, and called short cut.

--- src/output/REPL.java
+++ src/output/REPL.java
@@ -23,8 +23,19 @@ import java.util.Scanner;
 public class REPL {
     static Logger logger = Logger.getLogger(REPL.class);
     private final String HELP_STRING = "REPL Help:\n\n" +
-            "\nCommands:\n" +
-            "You must reference commands by their full path name. \nIf you want to create a node: pa.PA.createNode \nNotice that capitalization is important. \n\nOnly arguments that are aceeptable are Strings and Nodes. \nNodes have a @ sign before their title. Strings are surrounded in quotes. @esb \"Height\"";
+            "\nCommands:" +
+            "( help | ? ) - [SOLO] help text" +
+            "(     q    ) - [SOLO] quit the program" +
+            "(    end   ) - [SOLO] end the current converstation. Clears whiteboard and puts nodes." +
+            "(   like   ) - [INFIX] triggers xLikey" +
+            "(    is    ) - [INFIX] triggers xINHERITy" +
+            "(  create  ) - [PREFIX] creates a new node with default carrot headers" +
+            "(   view   ) - [PREFIX] prints the content of the node to stnd.out" +
+            "(   add    ) - [PREFIX] triggers Node.add" +
+            "(  called  ) - [INFIX] adds name to the specified node" +
+            "\n" +
+            "String arguments must be surrounded by quotation marks. - used in the add function." +
+            "Nodes are referenced by name.";
     private final String NODE_ID = "@";
     private Scanner input = new Scanner(System.in);
 
@@ -152,9 +163,9 @@ public class REPL {
                 Whiteboard.search(terms[0]);
             } else if (terms[0].contains(".") && terms.length > 1) {
                 parsedCommands = parseFull(command);
-            } else if (Core.contains(terms, "has") || Core.contains(terms, "is")) {
+            } else if (Core.contains(terms, "like") || Core.contains(terms, "is") || Core.contains(terms, "called")) {
                 //Infix Notation
-                if (Core.contains(terms, "has")) {
+                if (Core.contains(terms, "like")) {
                     command = command.replace("like", ",");
                     command = "logic.SetLogic.xLikey " + command;
                     parsedCommands = parseFull(command);
@@ -162,12 +173,16 @@ public class REPL {
                     command = command.replace("is", ",");
                     command = "logic.SetLogic.xINHERITy " + command;
                     parsedCommands = parseFull(command);
+                } else if (Core.contains(terms, "called")) {
+                    String[] calledSplit = command.split("called");
+                    command = "pa.Node.add " + calledSplit[0].trim() + ", \"^name\", \"" + calledSplit[1].trim() + "\"";
+                    parsedCommands = parseFull(command);
                 }
             } else {
                 //Prefix Notation
                 switch (terms[0]) {
                     case "createNode":
-                        Whiteboard.addNode(PA.createNode(command.replace("createNode", "").trim()));
+                        Whiteboard.addNode(PA.createNode(command.replace("create", "").trim()));
                         break;
                     case "view":
                         Core.println(viewNode(Whiteboard.search(command.replace("view", ""))));
